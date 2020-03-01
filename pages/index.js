ba3b200af34db8a27b6fca980fda1fab01a5c2ab
juicecultus <juicecultus@gmail.com>
2020-03-01 11:54:47 +0000

    style nested components separately or use global selectors

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,22 @@ const ShowLink = ({ show }) => (
     <Link href='/p/[id]' as={`/p/${show.id}`}>
       <a>{show.name}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: 'Arial';
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
