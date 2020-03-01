7df58df70232422274f05773c7d14247781f14fe
juicecultus <juicecultus@gmail.com>
2020-03-01 09:28:31 +0000

    use Clean URLs with Dynamic Routing

--- pages/index.js
+++ pages/index.js
@@ -10,21 +10,43 @@ import Link from 'next/link';
 ////
 
 // Method 2 - Page content as a prop
+// const PostLink = props => (
+//   <li>
+//     <Link href={`/post?title=${props.title}`}>
+//       <a>{props.title}</a>
+//     </Link>
+//   </li>
+// );
+
+// export default function Blog() {
+//   return (
+//     <Layout>
+//       <h1>My Blog</h1>
+//       <ul>
+//         <PostLink title='Hello Next.js' />
+//         <PostLink title='Learn Next.js is awesome' />
+//         <PostLink title='Deploy apps with Zeit' />
+//       </ul>
+//     </Layout>
+//   );
+// }
+
 const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
+    <Link href='/p/[id]' as={`/p/${props.id}`}>
+      <a>{props.id} </a>
     </Link>
   </li>
 );
+
 export default function Blog() {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink title='Hello Next.js' />
-        <PostLink title='Learn Next.js is awesome' />
-        <PostLink title='Deploy apps with Zeit' />
+        <PostLink id='hello-nextjs' />
+        <PostLink id='learn-nextjs' />
+        <PostLink id='deploy-nextjs' />
       </ul>
     </Layout>
   );
